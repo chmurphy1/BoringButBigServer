c44850cb22e7eab87033ed7e90aa23078eb86f72
Christopher Murphy <christopher.w.murphy1@gmail.com>
2019-04-04 19:40:25 -0600

    Added field to primary key on the workout table

--- BoringButBigBackEnd/src/main/java/com/christopherwmurphy/BoringButBigBackEnd/entities/Workout.java
+++ BoringButBigBackEnd/src/main/java/com/christopherwmurphy/BoringButBigBackEnd/entities/Workout.java
@@ -1,48 +1,32 @@
 package com.christopherwmurphy.BoringButBigBackEnd.entities;
 
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.christopherwmurphy.BoringButBigBackEnd.entities.keys.WorkoutPk;
 import com.christopherwmurphy.BoringButBigBackEnd.utilities.Constants;
 
 @Entity
 @Table(name = "workout")
 public class Workout {
 	
-	@Id
-	@Column(name = "workout_id")
-	private Integer workoutId;
-	
+	@EmbeddedId
+	WorkoutPk id;
+
 	@Column(name = "name")
 	private String name;
 	
-	@Column(name = "language")
-	private String language;
-	
 	public Workout() {
-		this.workoutId = 0;
 		this.name = Constants.EMPTY;
-		this.language = Constants.EMPTY;
 	}
 	
-	public Integer getWorkoutId() {
-		return workoutId;
-	}
-	public void setWorkoutId(Integer workoutId) {
-		this.workoutId = workoutId;
-	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public String getLanguage() {
-		return language;
-	}
-	public void setLanguage(String language) {
-		this.language = language;
 	}
 }
