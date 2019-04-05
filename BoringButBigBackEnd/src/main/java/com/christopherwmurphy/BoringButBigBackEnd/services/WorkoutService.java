4f82062da418f4c9aaa4cca03d3e45ed90c39658
Christopher Murphy <christopher.w.murphy1@gmail.com>
2019-04-04 19:48:35 -0600

    Updating service after change of workout table

--- BoringButBigBackEnd/src/main/java/com/christopherwmurphy/BoringButBigBackEnd/services/WorkoutService.java
+++ BoringButBigBackEnd/src/main/java/com/christopherwmurphy/BoringButBigBackEnd/services/WorkoutService.java
@@ -15,7 +15,7 @@ public class WorkoutService {
 	WorkoutRepository wRepo;
 	
 	public List<Workout> getWorkoutByLanguage(String lang){
-		return wRepo.findByLanguage(lang);
+		return wRepo.findByIdLanguage(lang);
 	}
 	
 	public List<Workout> getAllWorkouts() {
